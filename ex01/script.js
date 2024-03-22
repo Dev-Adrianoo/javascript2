97715c935f019438cb5895214f88f069a614cec7
Adriano Souza <Adrianoshadow733@gmail.com>
2024-03-22 10:27:46 -0300

    Exercício de javascript

---
+++ ex01/script.js
@@ -0,0 +1,20 @@
+// Utilize sempre Camel Case no JavaScript
+
+//Javascript possui tipagem dinâmica: infere os tipos de dados
+// JavaScript possuiu um tipagem fraca.
+
+var nomeDoProfessor = "Adriano Souza"; //String 
+var idade = 27; //number
+var altura = 1.82; //number
+var estudando  = true; //boolean (boleano: verdadeiro ou falso)
+
+console.log(`Nome do professor: "${nomeDoProfessor}" idade: "${idade}" altura do professor: "${altura}" e ele está estudando ? "${estudando}"`)
+
+
+var curso = "Front-End em React", 
+topico = "JavaScript Basico I";
+
+console.log(curso, topico)
+
+//! Não utilize o var nos seus códigos JavaScript
+
